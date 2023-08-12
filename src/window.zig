76df16534a6398892e3c622eaa07df75a203d1c4
Sergi PR <t3m1X@users.noreply.github.com>
2023-08-12 12:34:43 +0200

    Added window surface

--- src/window.zig
+++ src/window.zig
@@ -1,5 +1,7 @@
 const c = @import("c.zig");
-var window: ?*c.SDL_Window = null;
+
+pub var window: ?*c.SDL_Window = null;
+pub var surface: ?*c.SDL_Surface = null;
 
 pub fn init() !void {
     if (c.SDL_Init(c.SDL_INIT_VIDEO) != 0) {
@@ -18,6 +20,8 @@ pub fn init() !void {
         c.SDL_Log("Window could not be initialized: %s", c.SDL_GetError());
         return error.SDLInitializationFailed;
     }
+
+    surface = c.SDL_GetWindowSurface(window);
 }
 
 pub fn cleanUp() !void {
